ba4bcfa8227958dde00c1ee7adc01112466cee25
rghusbands <rghusbands@comcast.net>
2017-11-27 02:10:06 -0800

    Publishing first version of World's Greatest Leder Console Application

---
+++ LedgerConsoleApp/Driver.cs
@@ -0,0 +1,202 @@
+﻿/* 
+ * World's Greatest Banking Ledger Code Sample
+ * 
+ * Sample by: Robert Husbands
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedgerConsoleApp
+{
+    /* 
+     * Driver class runs console based ledger system.
+     * Loops to allow for any number of users and transactions to occur
+     * 
+     */
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            Run();
+            Console.WriteLine("Terminating...");
+        }
+
+        // Uses a while loop that collects user inputs indefinitely until users exit the application
+        static void Run()
+        {
+            // Boolean to keep system running
+            bool KeepGoing = true;
+
+            // Current user
+            UserAccount curUser = null;
+
+            // Simple intro
+            Console.WriteLine("Hello. Welcome to the World's Greatest Banking Ledger!" +
+                "\nNote: Commands are case sensitive.");
+
+            // Dictionary to hold user account information
+            Dictionary<string, UserAccount> userDict = new Dictionary<string, UserAccount>();
+
+            // While loop to allow the system to keep running until users Exit
+            while (KeepGoing)
+            {
+                // Ensure user is logged in before continuing further
+                if (curUser == null)
+                {
+                    Console.WriteLine("Currently logged out. Please log in before using commands!");
+                    curUser = Login(userDict);
+                }
+
+                // Inform user of available options 
+                Console.WriteLine("Commands are \"Deposit\", \"Withdrawal\", \"CheckBalance\", \"History\", " +
+                    "\"Logout\", and \"Exit\"");
+
+                // Get command from user
+                Console.Write("({0}) > ", curUser.username);
+                string cmd = Console.ReadLine();
+
+                // Completely terminate program with Exit       (POTENTIALLY CLEAR CACHE IDK)
+                if (cmd.Equals("Exit"))
+                {
+                    KeepGoing = false;
+                }
+
+                // Check if command is valid and perform desired UserAccount method
+                else if (cmd.Equals("Logout") || cmd.Equals("Deposit") || cmd.Equals("Withdrawal") ||
+                    cmd.Equals("CheckBalance") || cmd.Equals("History"))
+                {
+                    if (cmd.Equals("Logout"))
+                    {
+                        curUser = null;
+                        Console.WriteLine("Logged Out!");
+                    }
+
+                    if (cmd.Equals("Deposit"))
+                    {
+                        curUser.Deposit();
+                    }
+
+                    if (cmd.Equals("Withdrawal"))
+                    {
+                        curUser.Withdrawal();
+                    }
+
+                    if (cmd.Equals("CheckBalance"))
+                    {
+                        curUser.CheckBalance();
+                    }
+
+                    if (cmd.Equals("History"))
+                    {
+                        curUser.History();
+                    }
+
+                }
+
+                // Invalid command if it gets here
+                else
+                {
+                    Console.WriteLine("Invalid Command. Please try again.");
+                }
+            }
+        }
+
+        // Login method to handle user logins and ensure it's done properly
+        static UserAccount Login(Dictionary<string, UserAccount> dict)
+        {
+            Console.Write("Username (type \"Create\" to create account): ");
+            string userName = Console.ReadLine();
+
+            // Create account if user types "Create"
+            if (userName.Equals("Create"))
+            {
+                // Get a new username
+                Console.Write("Creating user account...\nType new username: ");
+                string newUser = Console.ReadLine();
+
+                // Make sure someone doesn't use the username "Create"
+                while (newUser.Equals("Create") || dict.ContainsKey(newUser))
+                {
+                    Console.WriteLine("Invalid username (Could already be in use)");
+                    Console.Write("Creating user account...\nType new username: ");
+                    newUser = Console.ReadLine();
+                }
+
+                // Confirm new password
+                string newPswd = NewPassword();
+
+                // Create user and return UserAccount
+                return CreateUserAccount(newUser, newPswd, dict);
+            }
+
+            // Check if username is in system
+            if (!dict.ContainsKey(userName))
+            {
+                Console.WriteLine("Username is not in system. Please try again");
+            }
+
+            // Get UserAccount if username is in system
+            else
+            {
+                // Get password from user
+                Console.Write("Password for {0}: ", userName);
+                string userPswd = Console.ReadLine();
+
+                // Get UserAccount from storage
+                UserAccount user = dict[userName];
+
+                // Check if the account password matches inputted password
+                if (user.CheckPassword(userPswd))
+                    return user;
+                else
+                    Console.WriteLine("Incorrect Password");
+            }
+
+            // Rerun login if fails
+            return Login(dict);
+        }
+
+        // Add a new user to UserAccount storage
+        static UserAccount CreateUserAccount(string name, string pswd, Dictionary<string, UserAccount> dict)
+        {
+            UserAccount user = new UserAccount(name, pswd);
+            dict.Add(name, user);
+            return user;
+        }
+
+        // Gets and confirms a new user password
+        static string NewPassword()
+        {
+            // Initialize passwords to be different
+            string pswd1 = "0", pswd2 = "1";
+
+            // Loop while passwords are different to ensure proper password input
+            while (!pswd1.Equals(pswd2))
+            {
+                // User inputs new password
+                Console.Write("Enter Password: ");
+                pswd1 = Console.ReadLine();
+
+                // User confirms new password
+                Console.Write("Confirm Password: ");
+                pswd2 = Console.ReadLine();
+                
+                // If passwords are different then notify user and go back through loop
+                if (!pswd1.Equals(pswd2))
+                {
+                    Console.WriteLine("Passwords didn't match. Please re-enter.");
+                }
+            }
+
+            // Return confirmed password
+            return pswd1;
+        }
+    }
+}
---
+++ LedgerConsoleApp/UserAccount.cs
@@ -0,0 +1,151 @@
+﻿/* 
+ * World's Greatest Banking Ledger Code Sample
+ * 
+ * Sample by: Robert Husbands
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedgerConsoleApp
+{
+    /* The UserAccount class is an object that holds all user variables and necessary methods
+     * Has methods to perform Deposit, Withdrawal, Check Balance, and Transaction History
+     */
+    public class UserAccount
+    {
+        // Necessary class variables
+        public string username { get; }
+        private string password { get; }
+        private decimal balance { get; set; }
+        private List<string> transactionsList { get; set; }
+
+        // UserAccount Contructor
+        public UserAccount(string name, string pswd)
+        {
+            this.username = name;
+            this.password = pswd;
+            this.balance = 0;
+            this.transactionsList = new List<string>();
+        }
+
+        // Deposit user defined quantity into account
+        public void Deposit()
+        {
+            Console.WriteLine("Starting a deposit...");
+            Console.Write("Enter the deposit amount: ");
+
+            // Grab user inputted deposit as a string
+            string str_deposit = Console.ReadLine();
+
+            // Attempt to convert input into a decimal. Catch failure if user inputs something that doesn't make sense
+            try
+            {
+                // Conversion from string to decimal
+                decimal deposit = decimal.Parse(str_deposit, System.Globalization.NumberStyles.Currency);
+
+                // Ensure input is a positive value
+                if (deposit < 0)
+                {
+                    throw new Exception();
+                }
+
+                // Add input to user balance
+                this.balance += deposit;
+
+                // Gather transaction information. Display and add to history
+                string transaction = DecimalToString(deposit) + " deposited";
+                this.AddTransaction(transaction);
+                Console.WriteLine(transaction + " into your account.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Deposit Failed. Please use positive numbers only.");
+            }
+        }
+
+        // Withdraw user definted quantity into account
+        public void Withdrawal()
+        {
+            Console.WriteLine("Starting a withdrawal...");
+            Console.Write("Enter the withdrawal amount: ");
+
+            // Grab user inputted withdrawal as a string
+            string str_withdrawal = Console.ReadLine();
+
+            // Attempt to convert input into a decimal. Catch failure if user inputs something that doesn't make sense
+            try
+            {
+                decimal withdrawal = decimal.Parse(str_withdrawal, System.Globalization.NumberStyles.Currency);
+                if (withdrawal < 0)
+                {
+                    throw new Exception();
+                }
+                this.balance -= withdrawal;
+                string transaction = DecimalToString(withdrawal) + " withdrawn";
+                this.AddTransaction(transaction);
+                Console.WriteLine(transaction + " from your account.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Withdrawal Failed. Please use positive numbers only.");
+            }
+        }
+
+        // Display current balance of user account
+        public void CheckBalance()
+        {
+            Console.WriteLine("Balance: {0}", DecimalToString(this.balance));
+        }
+
+        // Add a transaction to the transaction list
+        public void AddTransaction(string msg)
+        {
+            // Simply format the strings to show necessary information and combine them together
+            string transaction = "Transaction: " + msg;
+            string date = "Date: " + DateTime.Now.ToString("HH:mm:ss MM-dd-yyyy");
+            string curBalance = "Balance: " + DecimalToString(this.balance);
+            string report = transaction + "          " +
+                date + "         " + curBalance;
+
+            // Add to the current list
+            this.transactionsList.Add(report);
+        }
+
+        // Displays user transaction history
+        public void History()
+        {
+            Console.WriteLine("Checking history...");
+
+            // If empty notify the user that no transaction have occured yet
+            if (this.transactionsList.Count == 0)
+                Console.WriteLine("No transations yet");
+
+            // Iterate through transaction list and display transactions to user
+            foreach (string item in this.transactionsList)
+            {
+                Console.WriteLine(" - " + item);
+            }
+            Console.Write("\n");
+        }
+
+        // Simply convert decimal into a currency format
+        private static string DecimalToString(decimal num)
+        {
+            return ("$" + num.ToString("#,##0.00"));
+        }
+
+        // Simple password check when user tries to log in
+        public bool CheckPassword(string pswd)
+        {
+            if (pswd.Equals(this.password))
+                return true;
+            return false;
+        }
+    }
+}
